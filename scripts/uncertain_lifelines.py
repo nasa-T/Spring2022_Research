ddc450466fb6652c7ae7d7b4a46a35e4d3e4000a
nasa-T <tsmithgandy@gmail.com>
2022-02-16 23:23:44 -0500

    Units??

--- scripts/uncertain_lifelines.py
+++ scripts/uncertain_lifelines.py
@@ -24,9 +24,9 @@ z = 0.01 # dust-to-gas ratio
 h = 6.626E-34 # Planck's Constant
 c = 2.998E8 # speed of light
 kb = 1.3807E-23 # Boltzmann Constant
-opacity = 2.3 # dust opacity in m^2/g
+opacity = 2.3 # dust opacity in cm^2/g
 avgTemp = 20 # average disk temperature in Kelvin (Andrews et al., 2013)
-Mjup = 1.898E30
+Mjup = 1.898E30 # mass of jupiter in g
 
 # Planck blackbody function:
 B = lambda wavelength, temp: (2 * h * (c**2) / (wavelength**5)) / (np.e**(h*c/(wavelength*kb*temp))) 
@@ -35,4 +35,4 @@ mass = lambda F, distance: (F * distance**2) / (opacity * z * B(wl,20))
 
 rFlux = flux + flux_err * np.random.randn()
 rDist = dist + dist_err * np.random.randn()
-print(mass(rFlux, rDist)/ Mjup)
+print(mass(rFlux, rDist))
