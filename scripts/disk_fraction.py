aeadcb9d9e19919ebb10fb16891025aad08302dc
nasa-T <tsmithgandy@gmail.com>
2022-03-01 22:15:57 -0500

    Found ratios of Class III:II YSOs

--- scripts/disk_fraction.py
+++ scripts/disk_fraction.py
@@ -7,9 +7,10 @@ SOURCE = '~/Research/Spring2022_Research/ALMA_tight_binaries_Data_keepers2.csv'
 
 df = pd.read_csv(SOURCE)
 df = df.loc[:108] # non-data rows removed
-J = df.loc[:,'J']
-H = df.loc[:,'H']
-K = df.loc[:,'K']
+# JHK data for tight-binaries
+Jt = df.loc[:,'J']
+Ht = df.loc[:,'H']
+Kt = df.loc[:,'K']
 
 # Reading in non-tight-binary data
 from astropy.io import ascii
@@ -27,25 +28,34 @@ Kwise = ascii.read(NTFILE4)
 DaR2mHQ = DaR2mass[DaR2mass['ph_qual']=='AAA']
 DaRwHQ = DaRwise[DaRwise['ph_qual']=='AAAA']
 K2mHQ = K2mass[K2mass['ph_qual']=='AAA']
-K2wHQ = Kwise[Kwise['ph_qual']=='AAAA']
+KwHQ = Kwise[Kwise['ph_qual']=='AAAA']
 # Plot non-tight-binary data:
 plt.plot(DaR2mHQ['h_k'], DaR2mHQ['j_h'], 'ro', markersize=0.5)
 plt.plot(K2mHQ['h_k'], K2mHQ['j_h'], 'ko', markersize=0.5)
 # CTTS locus equation:
 j_h = lambda h, k: 0.58 * (h-k) + 0.52
-# Make dataframe for objects above CTTS locus:
-classII = (J-H) > j_h(H,K)
-df2 = df[classII]
+
 # Plot tight-binary data:
-plt.plot(H-K, J-H, 'o', markersize=4)
+plt.plot(Ht-Kt, Jt-Ht, 'o', markersize=4)
 # Plot CTTS locus:
-plt.plot(H-K, j_h(H,K), 'k-')
+plt.plot(Ht-Kt, j_h(Ht,Kt), 'k-')
 # Interstellar reddening vectors:
 ISR_vector = lambda x, c: 1.83 * x + c
 x1 = np.linspace(1.02/1.25,2.25,2)
 x2 = np.linspace(0.618/1.25,2.25,2)
+# Make dataframe for objects above ISR vector; Class III YSOs:
+classIIIDaR = DaR2mHQ[DaR2mHQ['j_h'] > ISR_vector(DaR2mHQ['h_k'],-0.098)]
+classIIIK = K2mHQ[K2mHQ['j_h'] > ISR_vector(K2mHQ['h_k'],-0.098)]
+classIIIt = df[(Jt-Ht) > ISR_vector(Ht-Kt,-0.098)]
+# Make dataframe for objects below ISR vector; Class II YSOs:
+classIIDaR = DaR2mHQ[DaR2mHQ['j_h'] < ISR_vector(DaR2mHQ['h_k'],-0.098)]
+classIIK = K2mHQ[K2mHQ['j_h'] < ISR_vector(K2mHQ['h_k'],-0.098)]
+classIIt = df[(Jt-Ht) < ISR_vector(Ht-Kt,-0.098)]
+print("Class III:II ratio for tight binary sample:",len(classIIIt)/len(classIIt))
+print("For DaRio 2016 sample:", len(classIIIDaR)/len(classIIDaR))
+print("For Kounkel 2016 sample:", len(classIIIK)/len(classIIK))
 # c values found from Robberto et al. (2010):
-plt.plot(x1, ISR_vector(x1, -0.5),'k-') 
+# plt.plot(x1, ISR_vector(x1, -0.5),'k-') 
 plt.plot(x2, ISR_vector(x2, -0.098),'k-')
 plt.legend(['DaRio2016', 'Kounkel2016', 'Tight-Binaries'])
 
