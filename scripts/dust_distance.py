90cf3729ab644c75dcaaf496e3d4aaa87a1a36ce
nasa-T <tsmithgandy@gmail.com>
2022-03-17 13:40:35 -0400

    Da Rio 2016 + Kounkel 2016 disk profiles

--- scripts/dust_distance.py
+++ scripts/dust_distance.py
@@ -59,6 +59,41 @@ ax4.set_xlabel('Distance of Emission (au)')
 ax4.set_ylabel('Magnitude of Emission')
 plt.show()
 
+# Reading in non-tight-binary WISE data
+from astropy.io import ascii
+FOLDER = '/Users/Celloman/Research/Spring2022_Research/infrareddata/'
+NTFILE1 = FOLDER+'DaRio16_wise.txt'
+NTFILE2 = FOLDER+'Kounkel16_wise.txt'
+
+DaRwise = ascii.read(NTFILE1)
+Kwise = ascii.read(NTFILE2)
+# Data with high quality:
+DaRwHQ = DaRwise[DaRwise['ph_qual']=='AAAA']
+KwHQ = Kwise[Kwise['ph_qual']=='AAAA']
+
+W1DaR = DaRwHQ['w1mpro']
+W2DaR = DaRwHQ['w2mpro']
+W3DaR = DaRwHQ['w3mpro']
+W4DaR = DaRwHQ['w4mpro']
+W1K = KwHQ['w1mpro']
+W2K = KwHQ['w2mpro']
+W3K = KwHQ['w3mpro']
+W4K = KwHQ['w4mpro']
+
+# Finding Average Teff of Da Rio 2016
+DaRio = pd.read_csv('~/Research/Spring2022_Research/DaRio2016_Teff.csv')
+DaRioTemp = np.mean(DaRio.loc[:,'Teff']) * u.K
+Kounkel = pd.read_csv('~/Research/Spring2022_Research/Kounkel2016_Teff.csv')
+KounkelTemp = np.mean(Kounkel.loc[:,'Teff']) * u.K
+dDaRW1 = d(DaRioTemp,Tdust(W1_wl))
+dDaRW2 = d(DaRioTemp,Tdust(W2_wl))
+dDaRW3 = d(DaRioTemp,Tdust(W3_wl))
+dDaRW4 = d(DaRioTemp,Tdust(W4_wl))
+dKW1 = d(DaRioTemp,Tdust(W1_wl))
+dKW2 = d(DaRioTemp,Tdust(W2_wl))
+dKW3 = d(DaRioTemp,Tdust(W3_wl))
+dKW4 = d(DaRioTemp,Tdust(W4_wl))
+
 # Making "profiles" of disks with all WISE bands
 # gather only objects with data from all WISE bands:
 df2 = df[df.loc[:,'W1'].notnull() & df.loc[:,'W2'].notnull() & df.loc[:,'W3'].notnull() & df.loc[:,'W4'].notnull()].reset_index()
@@ -72,13 +107,21 @@ d2W2 = d(Tstar2,Tdust(W2_wl))
 d2W3 = d(Tstar2,Tdust(W3_wl))
 d2W4 = d(Tstar2,Tdust(W4_wl))
 
-norm = mpl.colors.Normalize(vmin=3000, vmax=4500)
+norm = mpl.colors.Normalize(vmin=3000, vmax=6000)
 cmap = mpl.cm.hot
 sm = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
 for i in range(len(df2)):
     distances = np.array([d2W1.loc[i], d2W2.loc[i], d2W3.loc[i], d2W4.loc[i]])
     mags = np.array([W1_2.loc[i], W2_2.loc[i], W3_2.loc[i], W4_2.loc[i]])
     plt.plot(distances, mags, c=sm.to_rgba(Tstar2.loc[i]))
+for i in range(len(DaRwHQ)):
+    distances = np.array([dDaRW1.value, dDaRW2.value, dDaRW3.value, dDaRW4.value])
+    mags = np.array([W1DaR[i], W2DaR[i], W3DaR[i], W4DaR[i]])
+    plt.plot(distances, mags, 'g--', alpha=0.03)
+for i in range(len(KwHQ)):
+    distances = np.array([dKW1.value, dKW2.value, dKW3.value, dKW4.value])
+    mags = np.array([W1K[i], W2K[i], W3K[i], W4K[i]])
+    plt.plot(distances, mags, 'g--', alpha=0.03)
 plt.colorbar(sm, label='Teff', orientation='vertical')
 plt.gca().invert_yaxis()
 plt.xlabel('Distance of Emission (au)')
