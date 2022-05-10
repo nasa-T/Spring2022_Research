7b672d7134a2a488242139dfbd80fc376399100d
nasa-T <tsmithgandy@gmail.com>
2022-05-10 00:05:42 -0400

    functionality attained

--- scripts/operation_FindAlpha.py
+++ scripts/operation_FindAlpha.py
@@ -14,21 +14,55 @@ def rndm(a, b, g, size=1):
     r = np.random.random(size=size)
     ag, bg = a**g, b**g
     return (ag + (bg - ag)*r)**(1./g)
-F0 = min(df[detected].loc[:,'Flux (Jy)']) # flux of lowest detection for censoring
-n = 6 # when changing this, check dimensions of subplots
-α_range = np.linspace(-2,-0.05, n) # range of α values in question
-kmf = KaplanMeierFitter()
-for i in range(1,n+1):
-# for α in α_range:
-    α = round(α_range[i-1],2)
-    ax = plt.subplot(3,2,i)
-    # making a plot for each α value giving a false sample (based on power-law)
-    false_sample = rndm(min(flux), max(flux), g=α+1, size=len(df)) 
-    false_detected = false_sample > F0
-    kmf.fit(false_sample, false_detected, label='α = ' +str(α))
-    kmf.plot_cumulative_density(ax=ax)
-    kmf.fit(flux, detected, label='Actual Data')
-    kmf.plot_cumulative_density(ax=ax)
-    ax.set_xlabel('Flux (Jy)')
-plt.show()
+# sorted minimum detection fluxes for each object (5σ)
+F0 = np.sort(df.loc[:,'Flux_err']*5) 
+repetitions = 9
+α_range = np.linspace(-2,-0.05, repetitions) # range of α values in question
+def fake_v_realSample(realFlux,realDetected,fakeFlux,fakeDetected, axis,alpha='Fake Data'):
+    kmf = KaplanMeierFitter()
+    kmf.fit(fakeFlux, fakeDetected, label='α = ' +str(alpha))
+    kmf.plot_cumulative_density(ax=axis)
+    kmf.fit(realFlux, realDetected, label='Actual Data')
+    kmf.plot_cumulative_density(ax=axis)
+    axis.set_xlabel('Flux (Jy)')
+# ax = plt.subplot(1,1,1)
+# false_sample = np.sort(rndm(min(flux), max(flux), g=-1.1+1, size=len(df)))
+# false_detected = false_sample > F0
+# fake_v_realSample(flux,detected,false_sample,false_detected,ax)
+# plt.show()
+def closestFactorPair(num):
+    a = int(np.sqrt(num))
+    while (num % a) != 0:
+        a += 1
+    return (a, num//a)
+def findingAlpha(alphaRange,n=6):
+    for i in range(1,n+1):
+        α = round(α_range[i-1],2)
+        a, b = closestFactorPair(n)
+        ax = plt.subplot(a,b,i)
+        false_sample = np.sort(rndm(min(flux), max(flux), g=α+1, size=len(df)))
+        false_detected = false_sample > F0
+        fake_v_realSample(flux,detected,false_sample,false_detected,ax, alpha=α)
+    plt.show()
+findingAlpha(α_range,repetitions)
+# use randomized order for fake detection mask
+
+
+# for i in range(1, n+1):
+
+# kmf = KaplanMeierFitter()
+# for i in range(1,n+1):
+# # for α in α_range:
+#     α = round(α_range[i-1],2)
+#     ax = plt.subplot(3,2,i)
+#     # making a plot for each α value giving a false sample (based on power-law)
+#     # sort the false sample to compare to the sorted minimum detections
+#     false_sample = np.sort(rndm(min(flux), max(flux), g=α+1, size=len(df)))
+#     false_detected = false_sample > F0
+#     kmf.fit(false_sample, false_detected, label='α = ' +str(α))
+#     kmf.plot_cumulative_density(ax=ax)
+#     kmf.fit(flux, detected, label='Actual Data')
+#     kmf.plot_cumulative_density(ax=ax)
+#     ax.set_xlabel('Flux (Jy)')
+# plt.show()
 
