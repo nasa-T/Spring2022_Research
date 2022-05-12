a67fbb07eded24a5dc7ae9e718cf00d56b911f6f
nasa-T <tsmithgandy@gmail.com>
2022-05-12 14:44:46 -0400

    commentsssss

--- scripts/operation_FindAlpha.py
+++ scripts/operation_FindAlpha.py
@@ -19,6 +19,9 @@ F0 = np.sort(df.loc[:,'Flux_err']*5)
 repetitions = 9
 α_range = np.linspace(-2,-0.05, repetitions) # range of α values in question
 def fake_v_realSample(realFlux,realDetected,fakeFlux,fakeDetected, axis,alpha):
+    """Use the Kaplan Meier Fitter to plot the cumulative density of two sets 
+    of data on one axis; fake data is labeled with the α parameter of the power 
+    law [ N(F) = A*F^-α ]"""
     kmf = KaplanMeierFitter()
     kmf.fit(fakeFlux, fakeDetected, label='α = ' +str(alpha))
     kmf.plot_cumulative_density(ax=axis)
@@ -31,30 +34,51 @@ def fake_v_realSample(realFlux,realDetected,fakeFlux,fakeDetected, axis,alpha):
 # fake_v_realSample(flux,detected,false_sample,false_detected,ax)
 # plt.show()
 def closestFactorPair(num):
+    """Find the pair of factors of num that are closest together;
+    implemented here for axes sizing; 
+    avoid prime numbers...those are not welcome here;
+    returns pair of ints which are factors of num"""
+    # start with potential factor that would work if num is a perfect square
     a = int(np.sqrt(num))
     while (num % a) != 0:
+        # if it is not a factor, continue to check numbers until it is
         a += 1
+    # return pair of factors found
     return (a, num//a)
 def findingAlpha(alphaRange):
+    """Makes a bunch of plots of fake data for each alpha value and plots 
+    against real data; input an array of desired alpha values to look at; 
+    fruitless function that plots"""
+    # n: number of alpha values, plots, etc.
     n = len(alphaRange)
-    for i in range(1,n+1):
-        α = round(alphaRange[i-1],2)
-        a, b = closestFactorPair(n)
-        ax = plt.subplot(a,b,i)
+    for i in range(n):
+        α = alphaRange[i] # current α
+        a, b = closestFactorPair(n) # get dimensions for axes
+        ax = plt.subplot(a,b,i+1) # current axis
+        # generate false sample based on current α
         false_sample = np.sort(rndm(min(flux), max(flux), g=α+1, size=len(df)))
+        # create mask based on minimum detection limit of real sample
         false_detected = false_sample > F0
-        fake_v_realSample(flux,detected,false_sample,false_detected,ax, α)
+        # plot real and fake samples
+        fake_v_realSample(flux,detected,false_sample,false_detected,ax, round(α,2))
     plt.show()
 findingAlpha(α_range)
 # use randomized order for fake detection mask
 def randomDetectionMask(α=-1.1,n=6):
+    """Similar to findingAlpha but each iteration maintains the same α but 
+    randomizes the detection limit mask on each iteration"""
+    # generate false sample
     false_sample = np.sort(rndm(min(flux), max(flux), g=α+1, size=len(df)))
     for i in range(1,n+1): 
-        a, b = closestFactorPair(n)
-        ax = plt.subplot(a,b,i)
+        a, b = closestFactorPair(n) # get dimensions for axes
+        ax = plt.subplot(a,b,i) # current axis
         if i > 1:
+            # first false sample maintains order (not-randomized);
+            # all others are randomized
             np.random.shuffle(false_sample)
+        # create mask based on minimum detection limit of real sample
         false_detected = false_sample > F0
+        # plot real and fake samples
         fake_v_realSample(flux,detected,false_sample,false_detected,ax,α)
     plt.show()
 randomDetectionMask(-1.2, 12)
